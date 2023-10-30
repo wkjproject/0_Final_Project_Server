7f863a2a98cf9f4112838f1c0f9d0b4b23ad87a3
jwk <nakazjk@naver.com>
2023-10-30 17:04:43 +0900

    JWT의 장점을 살리기위한 리프레쉬토큰 생성

--- mongo.mjs
+++ mongo.mjs
@@ -19,47 +19,79 @@ mongoose
     console.error('MongoDB 연결 실패: ', error);
   });
 
-// JWT 토큰 생성
+// JWT 리프레쉬 토큰 생성
 usersSchema.methods.generateToken = function (cb) {
-  const tokenExpTime = '1h'; // jwt 토큰 만료시간 지정
+  // 리프레쉬토큰과 엑세스 토큰의 만료시간을 동일하게해 리프레쉬토큰의 보안을 강화
+  const refreshTokenExpTime = '10m'; // jwt 리프레쉬토큰 만료시간 지정
+  const accessTokenExpTime = '10m'; // jwt 엑세스토큰 만료시간 지정
   const tokenExp = new Date();
-  const secretKey = 'team6mongo';
-
-  tokenExp.setMinutes(tokenExp.getMinutes() + 60); // 현재 시간에 60분을 추가
+  const refreshSecretKey = 'team6mongoRefresh';
+  const accessSecretKey = 'team6mongoAccess';
+  tokenExp.setMinutes(tokenExp.getMinutes() + 10); // 현재 시간에 10분을 추가
   // 사용자의 ID를 토큰 페이로드로 설정합니다.
   const payload = {
     _id: this._id, // 예: 사용자의 MongoDB _id
   };
-  // 'team6mongo' 는 key로서 보안관리 필요
-  let token = jwt.sign(payload, secretKey, {
-    expiresIn: tokenExpTime,
+  // 'team6mongoRefresh' 는 key로서 보안관리 필요
+  const refreshToken = jwt.sign(payload, refreshSecretKey, {
+    expiresIn: refreshTokenExpTime,
   });
-  this.token = token;
+  const accessToken = jwt.sign(payload, accessSecretKey, {
+    expiresIn: accessTokenExpTime,
+  });
+  this.token = refreshToken;
   this.tokenExp = tokenExp;
   // mongoDB에 토큰 저장하는부분
   this.save()
     .then((data) => {
-      cb(null, data);
+      cb(null, data, accessToken);
     })
     .catch((err) => {
       cb(err);
     });
 };
 
-// 토큰 복호화
-
-usersSchema.statics.findByToken = async function (token) {
-  const thisUser = this;
-  const secretKey = 'team6mongo';
+// 엑세스 토큰 복호화, 엑세스 토큰 만료됐을때 리프레쉬토큰 쿠키로부터 받아와서 재발급
+usersSchema.statics.findByToken = async function (accessToken, refreshToken) {
+  const accessSecretKey = 'team6mongoAccess';
+  const refreshSecretKey = 'team6mongoRefresh';
   try {
-    const decoded = await jwt.verify(token, secretKey); // _id 기반으로 jwt토큰을 만들었기때문에 디코드하면 _id가 나옴
-    const user = await thisUser.findOne({
-      _id: decoded._id,
-      token: token,
-    });
-    return user;
+    const decoded = await jwt.verify(accessToken, accessSecretKey);
+    return decoded._id;
   } catch (err) {
     console.error('Error in findByToken:', err);
+
+    // 만료된 경우 리프레시 토큰을 검사하고 새로운 엑세스 토큰 발급
+    if (err.name === 'TokenExpiredError') {
+      if (refreshToken) {
+        try {
+          // 리프레쉬 코인 만료여부 조사
+          const decodedRefreshToken = await jwt.verify(
+            refreshToken,
+            refreshSecretKey
+          );
+          // 리프레쉬토큰을 decode 한 결과의 _id와 서버에 저장된 refresh 토큰의 일치여부를 확인
+          const user = await this.findOne({
+            _id: decodedRefreshToken._id,
+            token: refreshToken,
+          });
+          if (user) {
+            // 리프레시 토큰이 유효한 경우, 새로운 엑세스 토큰 발급
+            const newAccessToken = jwt.sign(
+              { _id: decodedRefreshToken._id },
+              accessSecretKey,
+              { expiresIn: '10m' }
+            );
+            // 새로운 엑세스 토큰을 클라이언트에게 보내줍니다 (예: res.cookie('accessToken', newAccessToken))
+            return newAccessToken;
+          } else {
+            console.error('User not found for refreshToken:', refreshToken);
+          }
+        } catch (refreshTokenError) {
+          console.error('Error in verifying refreshToken:', refreshTokenError);
+        }
+      }
+    }
     return null;
   }
 };
--- server.mjs
+++ server.mjs
@@ -16,7 +16,12 @@ import { randomCode } from './function/GenRandomCode.mjs';
 
 const port = 5000;
 const app = express();
-app.use(cors({}));
+app.use(
+  cors({
+    origin: true, // 출처 허용 옵션
+    credentials: true, // 사용자 인증이 필요한 리소스(쿠키 등) 접근
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -44,20 +49,40 @@ app.post('/login', async (req, res) => {
       userFind.userPassword
     );
     if (checkUserPwd) {
-      await userFind.generateToken((err, data) => {
+      await userFind.generateToken((err, data, accessToken) => {
         if (err) return res.status(400).send(err);
+        // 리프레쉬토큰을 쿠키에 저장
+        res
+          .cookie('refreshToken', userFind.token, {
+            httpOnly: true, // HTTP Only 설정
+            secure: true, // HTTPS에서만 사용하도록 설정 (Production 환경에서)
+          })
+          .status(200)
+          .json({
+            loginSuccess: true,
+            message: '로그인 성공',
+            accessToken: accessToken,
+            userName: userFind.userName,
+            userAddr: userFind.userAddr,
+            userPhoneNum: userFind.userPhoneNum,
+            userMail: userFind.userMail,
+            _id: userFind._id,
+            userId: userFind.userId,
+            isAdmin: userFind.role === 0 ? false : true, // role이 0이면 일반사용자, 0이아니면 운영자
+            isLogin: true,
+          });
         // token을 클라이언트로 보냄
-        res.status(200).json({
+        /*         res.status(200).json({
           loginSuccess: true,
           message: '로그인 성공',
-          token: userFind.token,
+          accessToken: accessToken,
           userName: userFind.userName,
           userAddr: userFind.userAddr,
           userPhoneNum: userFind.userPhoneNum,
           userMail: userFind.userMail,
           _id: userFind._id,
           userId: userFind.userId,
-        });
+        }); */
       });
     } else {
       res.status(200).json({
@@ -589,17 +614,13 @@ app.post('/modifyProj', async (req, res) => {
 // 사용자 인증부분
 app.get('/auth', middleAuth, async (req, res) => {
   try {
-    // 사용자 _id(몽고DB 고유 _id) + 로컬스토리지 토큰 과 서버에 있는 _id + 토큰을 비교해 일치할경우 다음과같은 응답
-    res.status(200).json({
-      _id: req.foundUser._id, // middleAuth 에서 제공한 foundUser
-      userId: req.foundUser.userId,
-      isAdmin: req.foundUser.role === 0 ? false : true, // role이 0이면 일반사용자, 0이아니면 운영자
-      isLogin: true,
-      userAddr: req.foundUser.userAddr,
-      userName: req.foundUser.userName,
-      userPhoneNum: req.foundUser.userPhoneNum,
-      userMail: req.foundUser.userMail,
-    });
+    if (req.isLogin === false) {
+      // 인증에 실패한 경우
+      res.status(200).json({ isLogin: false });
+    } else {
+      // 인증에 성공한 경우
+      res.status(200).json({ isLogin: true });
+    }
   } catch (err) {
     console.log('server.mjs', err);
   }
@@ -618,8 +639,8 @@ app.get('/projName', async (req, res) => {
 // 프로젝트 모두 가져오는 예시
 app.get('/projects', async (req, res) => {
   try {
-    const projName = await projects.find({});
-    res.status(200).json({ projName });
+    const AllProjects = await projects.find({});
+    res.status(200).json({ projects: AllProjects });
   } catch (err) {
     console.log(err);
   }
